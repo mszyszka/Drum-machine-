fb8e2b8768af0daef9027c11f603e035980ee261
Michal <mszyszka223@gmail.com>
2019-03-19 17:09:23 +0100

    Im working on createPads()

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const padsBtn = document.querySelector('.padsButton');
 padsBtn.addEventListener('click', function() {
-    console.log('DZIAŁA');
+    const ul = document.querySelector('.padsUl');
+    ul.classList.toggle('listDisplay');
 });
+padsBtn.addEventListener('click', createPads);
+
+//Po kliknięciu na elemnet li - x, chcę odpalić funkcję do której przekażę wartość
+// elementu x jako argument. Ta funkcja stworzy plansze z ilościę padów równą x.
+
+//1. Potrzebuję na każdy li zapiąć addEventListener
+
+function createPads(){
+    const listItems = document.querySelectorAll('.padsLi');
+    const listItemsLength = listItems.length;
+    
+    for (let i = 0; i <= listItemsLength; i++) {
+        listItems.item(i).addEventListener('click', function(e){
+            
+            console.log(e.target.childNodes.text);
+        })
+    }
+}
+
+
 
 function sound(e){
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
@@ -20,7 +41,6 @@ function removePlayingCSS(e){
     pad.classList.remove('playing');
     
 }
+
 window.addEventListener('keydown', sound);
 window.addEventListener('keyup', removePlayingCSS);
-
-
