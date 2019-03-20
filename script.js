b8e09ee47cda6b571023263cdb9128d9cd54bd90
Michal <mszyszka223@gmail.com>
2019-03-20 10:23:23 +0100

    I structurized code to make it more easier to read and work with it.

--- script.js
+++ script.js
@@ -1,50 +1,60 @@
-const padsBtn = document.querySelector('.padsButton');
-padsBtn.addEventListener('click', function() {
-    const ul = document.querySelector('.padsUl');
-    ul.classList.toggle('listDisplay');
-});
-padsBtn.addEventListener('click', createPads);
-
 //Po kliknięciu na elemnet li - x, chcę odpalić funkcję do której przekażę wartość
 // elementu x jako argument. Ta funkcja stworzy plansze z ilościę padów równą x.
 
 //1. Potrzebuję na każdy li zapiąć addEventListener - done
 //2.Potrzebuje do funkcji odpalanej na click przekazać event - done
-//3.Potrzębuję napisać kod który stworzy plansze z ilościę padów równą wartości liValue
+//3.Potrzębuję napisać kod który stworzy plansze z ilościę padów równą wartości liValue - done
 
-function createPads(){
+
+//Toggle .listDisplay on .padsUl element
+function displayingPadsUl() {
+    const ul = document.querySelector('.padsUl');
+    ul.classList.toggle('listDisplay');
+}
+
+const padsBtn = document.querySelector('.padsButton');
+//After click on .padsButton execute displayingPadsUl()
+padsBtn.addEventListener('click', displayingPadsUl);
+padsBtn.addEventListener('click', addClickEventToListItems);
+
+
+
+function createPadsElements(e){
+            
+    let target = e.target;
+    let firstChild = target.firstChild;
+    let numberOfpads = firstChild.data;
+    
+    let drumPadsContainer = document.getElementsByClassName('drum-pads-container')[0];
+
+    //Create pad element and add to it .drum-pad
+    let padElement = document.createElement('div');
+    padElement.classList.add('drum-pad');
+
+    //Close .padsUl when user clicks on listItem
+    document.querySelector('.padsUl').classList.remove('listDisplay');
+
+    //Remove childs of drumPadsElemnt when user clicks on listItem to show only pointed number of pads
+    // instead of add extra pads to already existing.
+    while (drumPadsContainer.hasChildNodes()) {
+       drumPadsContainer.removeChild(drumPadsContainer.firstChild);
+    }
+
+    for(let x = 0; x < numberOfpads; x++) {
+        //Add to .drum-pads-container padElement's
+        drumPadsContainer.appendChild(padElement.cloneNode());
+    }
+    
+}
+
+function addClickEventToListItems(){
     const listItems = document.querySelectorAll('.padsLi');
     const listItemsLength = listItems.length;
-    
+
     for (let i = 0; i <= listItemsLength; i++) {
-        listItems.item(i).addEventListener('click', function(e){
-            
-            let target = e.target;
-            let firstChild = target.firstChild;
-            let numberOfpads = firstChild.data;
-            
-            let drumPadsContainer = document.getElementsByClassName('drum-pads-container')[0];
-
-            //Create pad element and add to it .drum-pad
-            let padElement = document.createElement('div');
-            padElement.classList.add('drum-pad');
-
-            //Close .padsUl when user clicks on listItem
-            document.querySelector('.padsUl').classList.remove('listDisplay');
-
-            //Remove childs of drumPadsElemnt when user clicks on listItem to show only pointed number of pads
-            // instead of add extra pads to already existing.
-            while (drumPadsContainer.hasChildNodes()) {
-               drumPadsContainer.removeChild(drumPadsContainer.firstChild);
-            }
-
-            for(let x = 0; x < numberOfpads; x++) {
-                //Add to .drum-pads-container padElement's
-                drumPadsContainer.appendChild(padElement.cloneNode());
-            }
-            
-        });
+        listItems.item(i).addEventListener('click', createPadsElements);
     }
+
 }
 
 
